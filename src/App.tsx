fa3c550a45769485708a0d0302d4d30008a66aa9
Jose B <jose.zdy@gmail.com>
2021-07-22 08:45:12 -0500

    da base

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/image/fist_nobg.png';
+// import bgVideo from './assets/video/anim.mp4';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <video autoPlay muted loop playsInline id="myVideo">
+        <source src={bgVideo} type="video/mp4" />
+      </video> */}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+        </header>
+      </div>
+    </>
   );
 }
 
