30c90e7da9492c677e8f081b0d1219f62cae828f
Jose B <jose.zdy@gmail.com>
2021-07-22 12:48:06 -0500

    background video

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import logo from './assets/image/fist_nobg.png';
-// import bgVideo from './assets/video/anim.mp4';
+import bgVideo from './assets/video/anim.mp4';
 import './App.css';
 
 function App() {
   return (
-    <>
-      {/* <video autoPlay muted loop playsInline id="myVideo">
+    <div style={{
+      position: "relative",
+      overflow: "hidden",
+    }}>
+      <video autoPlay muted loop playsInline id="myVideo" style={{
+        position: "absolute",
+        width: "100%",
+        top: "50%",
+        transform: "translateY(-50%)",
+      }}>
         <source src={bgVideo} type="video/mp4" />
-      </video> */}
+      </video>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
 
         </header>
       </div>
-    </>
+    </div>
   );
 }
 
